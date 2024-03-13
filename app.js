1cb33508efc5916f04ef0467ce280350e72594c1
JJ1312 <yeiyeithirteen@gmail.com>
2024-03-13 13:34:42 -0300

    Finish board display

--- app.js
+++ app.js
@@ -11,6 +11,16 @@ const createBoard = (size) => {
 }
 
 const displayBoard = (board) => {
+
+  // Vertical lines
+  document.styleSheets[0].insertRule(`.cell:nth-child(-n + ${boardDim})::before{height: 50%; top:50%}`);
+  document.styleSheets[0].insertRule(`.cell:nth-last-child(-n + ${boardDim})::before{height: 50%; }`);
+
+  // Horizontal lines
+  document.styleSheets[0].insertRule(`.cell:nth-child(${boardDim}n)::after{width: 50%;}`);
+  document.styleSheets[0].insertRule(`.cell:nth-child(${boardDim}n + 1)::after{width: 50%; left: 50%;}`);
+
+  // Cells
   for(let row in board){
     for(let id in board[row]){
       let div = document.createElement('div');
@@ -22,11 +32,12 @@ const displayBoard = (board) => {
 }
 
 let boardDim = 9;
-let cellSize = 50;
+let cellSize = 30;
 
 document.querySelector(':root').style = `--size-cell: ${cellSize}px`;
 document.getElementById('board').style.gridTemplateColumns = `repeat(${boardDim}, var(--size-cell))`;
 
 let board = createBoard(boardDim);
 displayBoard(board);
+
 // console.table(board);
